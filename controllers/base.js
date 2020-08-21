00a850d6743fe2434c31675f65ba47e56018d166
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-21 19:30:56 -0400

    added return status for base routes

--- controllers/base.js
+++ controllers/base.js
@@ -2,14 +2,14 @@ const db = require('../models');
 
 
 const home = (req, res) => {
-  console.log('</h1>Home Route</h1>');
+  return res.status(200);
 }
 const about = (req, res) => {
-  res.send('</h1>About Route</h1>');
+  return res.status(200);
 };
 
 const contact = (req, res) => {
-  res.send('</h1>Contact Route</h1>');
+  return res.status(200);
 };
 
 
