6429b9c0e0ea4c6062d4ebc603ec8bf084ca9fb7
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-23 08:40:28 -0400

    Changed Home Routes- route point

--- routes/base.js
+++ routes/base.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const ctrl = require('../controllers/');
 
 
-router.get('/home', ctrl.base.home);
+router.get('/', ctrl.base.home);
 router.get('/about', ctrl.base.about);
 router.get('/contact', ctrl.base.contact)
 
