5d31e8aee1344ead191ce8fc7c548d0013ff9df1
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-23 11:03:15 -0400

    playing around with mongodb connection

--- seed.js
+++ seed.js
@@ -2,15 +2,6 @@ const db = require('./models');
 const data = require('./postsData.json');
 const data1 = require('./userData.json');
 
-db.Post.deleteMany({}, (err, deletePosts) => {
-  db.Post.create(data.posts, (err, seededPosts) => {
-    if (err) console.log(err);
-    console.log(data.posts.length, 'Posts created successfully');
-    console.log(deletePosts);
-    process.exit();
-  });
-});
-
 db.User.deleteMany({}, (err, deleteUsers) => {
   db.User.create(data1.users, (err, seededUsers) => {
     if (err) console.log(err);
@@ -19,3 +10,14 @@ db.User.deleteMany({}, (err, deleteUsers) => {
     process.exit();
   });
 });
+db.Post.deleteMany({}, (err, deletePosts) => {
+  db.Post.create(data.posts, (err, seededPosts) => {
+    if (err) console.log(err);
+    console.log(data.posts.length, 'Posts created successfully');
+    console.log(deletePosts);
+    process.exit();
+  })
+
+});
+
+
